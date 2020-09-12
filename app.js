82d147d8249295bb5fc30a6efd4affe4d60faac5
Vitaly Marom <48221011+VitalyMarom@users.noreply.github.com>
2020-09-12 22:50:30 +0300

    uploaded web files
    nodeJS website

---
+++ app.js
@@ -0,0 +1,33 @@
+
+const express = require('express');
+var bodyParser = require('body-parser');
+const database = require('./database.json');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json())
+
+app.get("/getdata",(request,response)=>{
+    response.status(200);
+    response.send(database);
+})
+
+app.put("/edit/:c",
+    (request, response) => {
+
+        let country = database.find(db => db.name == request.params.c);
+
+        if (country != undefined) {
+            country.counter = request.body.counter;
+        }
+
+        response.status(200); // 200 is OK
+        response.send();
+    }
+);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
---
+++ code.js
@@ -0,0 +1,51 @@
+
+        function getData(){
+        //Insert all countries to the select box
+        fetch("/getdata")
+        .then(res=>res.json())
+        .then(data=>{
+            document.getElementById("table").innerHTML=" ";
+            data.forEach(element => {
+                document.getElementById("counteries").innerHTML+=`<option value="${element.name}">
+                ${element.name}
+                </option>`;
+                document.getElementById("table").innerHTML+=`
+            <tr>
+            <td>${element.name}</td>
+            <td>${element.population}</td>
+            <td>${element.counter}</td>
+            </tr>`
+            });
+        })
+        .catch(err=>console.log("Error",err));
+        }
+
+        /*db.forEach(element=>{
+            document.getElementById("table").innerHTML+=`
+            <tr>
+            <td>${element.name}</td>
+            <td>${element.population}</td>
+            <td>${element.counter}</td>
+            </tr>`
+        }); */
+
+        /*function getcountrydata(){
+            //send the county you selected to the server
+            let select = document.getElementById("counteries");
+            let country = select.options[select.selectedIndex].value;
+            console.log(country);
+        }*/
+
+        async function updateCounter(){
+            let counter = document.getElementById("cases").value;
+            let select = document.getElementById("counteries");
+            let country = select.options[select.selectedIndex].value;
+            let initParam= { 
+                "method":"PUT",
+                 headers: {"Content-Type": "application/json"},
+                 body:`{"counter":"${counter}"}`
+                };
+            let res= await (fetch(`/edit/${country}`,initParam));
+            console.log(res);
+            //getData();
+        }
